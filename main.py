4da7036e9fd4c99f9d6a1162a216750d55bc1a1a
Alyssa Wu <alyssawu@Alyssas-MacBook-Air.local>
2024-12-02 15:21:39 -0500

    potential steps for our experimentation

---
+++ main.py
@@ -0,0 +1,38 @@
+def english_preprocess(sentence):
+    '''
+    Lowercasing
+    Remove punctuation (not needed for mBERT but might need for XLM-R)
+    Tokenization
+    '''
+    pass
+
+def french_preprocess(sentence):
+    '''
+    Lowercasing
+    Removing accents
+    Remove punctuation (not needed for mBERT but might need for XLM-R)
+    Tokenization
+    '''
+    pass
+
+'''
+USING THE MODEL
+1. Tokenize preprocessed sentences
+2. Extract embeddings using mBERT or XLM-R
+
+USING THE PROBE
+The probe needs embeddings as input, and corresponding syntactic properties as output/labels
+0. Split the embeddings into training, development, and test set?
+1. Get syntactic annotations like parse tree distances or depths, derived from the corpus.
+        (this might be in the xml file of the corpus, or we have to do this manually. how? idk.)
+2. Train the structural probe (the one linked in final project proposal)
+        (modify the parameters of the probe to see which one has the best result)
+3. Evaluate the probe on the test set
+        (use spearman correlation, accuracy, etc)
+4. Run the probe on both english and french embeddings
+
+EVALUATING RESULTS
+Syntactic Differences:
+1. Analyze how well embeddings from each language encode syntactic properties.
+Differences in Spearman correlation can reveal cross-linguistic syntactic variations.
+'''
